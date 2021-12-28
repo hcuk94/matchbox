52b9580343d4c3d899831b4f1d047314c4d242cc
hcuk94 <henry@henrycole.uk>
2021-12-28 17:10:22 +0000

    added sample config file

---
+++ config_sample.py
@@ -0,0 +1,17 @@
+# ScrobbleBox Sample Config File
+
+# Last.fm Account/API Config
+lastfm_api_key = 'CHANGEME'
+lastfm_api_secret = 'CHANGEME'
+lastfm_username = 'CHANGEME'
+lastfm_password = 'CHANGEME'
+
+# Audd.io API Config
+audd_api_key = 'CHANGEME'
+
+# Recorder Config
+recorder_silence_threshold = 500
+recorder_record_seconds = 10
+recorder_chunk = 1024
+recorder_channels = 1
+recorder_rate = 44100
