05527d01d2fa365263c092663eb2498e68b0403b
hcuk94 <me@hc.me.uk>
2021-12-29 15:09:09 +0000

    moving to proper logging

--- main.py
+++ main.py
@@ -3,27 +3,50 @@ import lastfm
 import audd
 import acrcloud
 import recorder
+import logging
+from time import sleep
 
 if __name__ == '__main__':
-    recording = recorder.Recording()
-    recording.do_recording()
-    recording.check_if_silent()
-    if recording.check_if_silent() is False:
-        recording.save_file()
-        file = recording.wave_filename
-        if config.mrt_api == 'acrcloud':
-            track_match = acrcloud.ApiReq(file)
-        else:
-            track_match = audd.ApiReq(file)
-        match_result = track_match.match_file()
-        scrobble = lastfm.Scrobbler(match_result)
-        scrobble.now_playing()
-        scrobble.scrobble()
-        print("Successfully scrobbled {} by {} from album {}".format(match_result['title'],
-                                                                     match_result['artist'], match_result['album']))
-        recording.close_stream()
-        recording.del_file()
-    else:
-        print("Recording has been deemed silent, so we will do nothing.")
-        recording.close_stream()
+    logging.basicConfig(level=config.log_level, filename=config.log_filename
+                        , filemode="a+", format="%(asctime)-15s %(levelname)-8s %(message)s")
 
+    logging.info("Application Started")
+    while True:
+        logging.debug("Initialising Recorder")
+        recording = recorder.Recording()
+        logging.debug("Recording {} seconds of audio...".format(config.recorder_record_seconds))
+        recording.do_recording()
+        logging.debug("Recording complete.")
+        logging.debug("Checking if recording is silent...")
+        is_silent = recording.check_if_silent()
+        if is_silent is False:
+            logging.debug("Recording is not silent.")
+            logging.debug("Saving temporary file...")
+            recording.save_file()
+            file = recording.wave_filename
+            logging.debug("File saved: {}".format(file))
+            logging.debug("Checking MRT API option...")
+            if config.mrt_api == 'acrcloud':
+                logging.debug("Matching track using API {}".format(config.mrt_api))
+                track_match = acrcloud.ApiReq(file)
+            else:
+                logging.debug("Matching track using API {}".format(config.mrt_api))
+                track_match = audd.ApiReq(file)
+            match_result = track_match.match_file()
+            logging.debug("MRT API Output: {}".format(match_result))
+            logging.debug("Initialising scrobbler...")
+            scrobble = lastfm.Scrobbler(match_result)
+            logging.debug("Updating 'now playing'...")
+            scrobble.now_playing()
+            logging.debug("Scrobbling track...")
+            scrobble.scrobble()
+            logging.info("Successfully scrobbled {} by {} from album {}".format(match_result['title'],
+                                                                         match_result['artist'], match_result['album']))
+            recording.close_stream()
+            logging.debug("Deleting temporary file {}...".format(file))
+            recording.del_file()
+        else:
+            logging.info("Recording was deemed as silent, no action will be taken.")
+            recording.close_stream()
+        logging.debug("Sleeping... next run in {} seconds".format(config.retrigger_time))
+        sleep(config.retrigger_time)
