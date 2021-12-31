571c0ec7aa92d041d182dff3068b96df8031110f
hcuk94 <me@hc.me.uk>
2021-12-31 16:15:55 +0000

    updated sample conf to reflect notify package added

--- config_sample.py
+++ config_sample.py
@@ -27,15 +27,22 @@ providers_match = {
     }
 }
 
-# Last.fm Account/API Config
-lastfm_api_key = ''
-lastfm_api_secret = ''
-lastfm_username = ''
-lastfm_password = ''
+# Providers - Notify
+providers_notify = {
+    'LastFM': {
+        'enabled': True,
+        'config': {
+            'api_key': '',
+            'api_secret': '',
+            'username': '',
+            'password': ''
+        }
+    }
+}
 
 # Recorder Config
 recorder_silence_threshold = 500
-recorder_record_seconds = 10
+recorder_record_seconds = 15
 recorder_chunk = 1024
 recorder_channels = 1
 recorder_rate = 44100
